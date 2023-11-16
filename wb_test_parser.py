460b53d2bc06c6dc5a58ddf7816a55b09fc7abec
Andrey Marach <amarach@utracesolutions.com>
2023-11-16 23:15:35 +0300

    feat: modify test file

---
+++ wb_test_parser.py
@@ -0,0 +1,40 @@
+import logging
+import requests
+import bs4
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger('wb')
+
+
+class Client():
+
+    def __init__(self):
+        self.session = requests.Session()
+        self.session.headers = {
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
+            'Accept-Language': 'ru',
+        }
+
+    def load_page(self,page: int = None):
+        url = 'https://www.wildberries.ru/catalog/muzhchinam/odezhda/rubashki'
+        res = self.session.get(url=url)
+        res.raise_for_status()
+        return res.text
+
+    def parse_page(self, text: str):
+        soup = bs4.BeautifulSoup(text, 'lxml')
+        container = soup.select('div.product-card__wrapper')
+        for block in container:
+            self.parse_block(block=block)
+
+    def parse_block(self, block):
+        logger.info(block)
+        logger.info('=' * 100)
+
+    def run(self):
+        text = self.load_page()
+        self.parse_page(text=text)
+
+if __name__ == '__main__':
+    parser = Client()
+    parser.run()
