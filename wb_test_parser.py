4721a52c010e186d0ed96c64912e2abe7aab1a76
Andrew Marach <marach@sitedtech.com>
2024-10-05 00:06:23 +0300

    feat: добавил селениум и сделал тестовый гет запрос

--- wb_test_parser.py
+++ wb_test_parser.py
@@ -1,10 +1,35 @@
 import logging
 import requests
 import bs4
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.wait import WebDriverWait
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger('wb')
 
+#у селениума бывают два типа ожиданий : explicit - явные , implicit - неявные
+
+# у вайлдберриз динамический контент на сайте и нужно юзать Селениум
+# с помощью селениума будем добывать данные
+# c помощью супа будем их парсить и производить действия
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
+URI = "https://www.wildberries.ru/catalog/muzhchinam/odezhda/rubashki"
+
+
+def get_source_code(URI: str) -> None:
+
+    driver.get(url=URI)
+    # time.sleep(10)
+    while True:
+        try:
+            element = WebDriverWait(driver=driver, timeout=2)
+
+def main() -> None:
+    get_source_code(URI)
 
 class Client():
 
@@ -16,14 +41,14 @@ class Client():
         }
 
     def load_page(self,page: int = None):
-        url = 'https://www.wildberries.ru/catalog/muzhchinam/odezhda/rubashki'
+        url = 'https://serveradmin.ru'
         res = self.session.get(url=url)
         res.raise_for_status()
         return res.text
 
     def parse_page(self, text: str):
         soup = bs4.BeautifulSoup(text, 'lxml')
-        container = soup.select('div.product-card__wrapper')
+        container = soup.select('div')
         for block in container:
             self.parse_block(block=block)
 
@@ -35,6 +60,8 @@ class Client():
         text = self.load_page()
         self.parse_page(text=text)
 
+# if __name__ == '__main__':
+#     parser = Client()
+#     parser.run()
 if __name__ == '__main__':
-    parser = Client()
-    parser.run()
+    main()
